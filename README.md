e0abaa0300abaf11edd75ad994c91fe51c174c8c
Nikola Ljubešić <nljubesi@gmail.com>
2017-02-25 09:40:43 +0100

    Updated extraction script to produce lists by part-of-speech

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # lexicon
-Method for extracting lexical samples for further annotation
+Method for extracting lexical samples for further annotation.
+
+We extract the most frequent lexemes of Croatian from the hrLex v1.2 lexicon (http://hdl.handle.net/11356/1072) which is
+
+1. built by maximizing the coverage of the hrWaC corpus (http://hdl.handle.net/11356/1064, http://nl.ijs.si/noske/all.cgi/first_form?corpname=hrwac;align=)
+
+2. enriched with frequencies from the hrWaC corpus
+
+The extraction process is defined in ```sample.py```. To run it, simply give the script as an argument to your python (2.*) interpreter:
+
+```
+python sample.py
+```
--- sample.py
+++ sample.py
@@ -6,6 +6,22 @@ for line in gzip.open('hrLex_v1.2.gz'):
   lexeme=lemma+'#'+tag[:2]
   lexemefreq[lexeme]=lexemefreq.get(lexeme,0)+int(freq)
 outf=open('lexeme_frequencies.txt','w')
+outfnc=open('nc_frequencies.txt','w')
+outfag=open('ag_frequencies.txt','w')
+outfvm=open('vm_frequencies.txt','w')
+outfas=open('as_frequencies.txt','w')
 for lexeme,freq in sorted(lexemefreq.items(),key=lambda x:-x[1]):
   outf.write(lexeme+'\t'+str(freq)+'\n')
+  if lexeme.endswith('#Nc'):
+    outfnc.write(lexeme+'\t'+str(freq)+'\n')
+  elif lexeme.endswith('#Ag'):
+    outfag.write(lexeme+'\t'+str(freq)+'\n')
+  elif lexeme.endswith('#Vm'):
+    outfvm.write(lexeme+'\t'+str(freq)+'\n')
+  elif lexeme.endswith('#As'):
+    outfas.write(lexeme+'\t'+str(freq)+'\n')
 outf.close()
+outfnc.close()
+outfag.close()
+outfvm.close()
+outfas.close()
