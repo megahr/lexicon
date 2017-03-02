d91a40f97642a81e98fda284fe161b3239c80c1f
Nikola Ljubešić <nljubesi@gmail.com>
2017-03-02 13:01:14 +0100

    Updated sample.py to generate _shuf.txt files for human inspection of first 1000

--- sample.py
+++ sample.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys,gzip
+from random import shuffle
 lexemefreq={}
 for line in gzip.open('hrLex_v1.2.gz'):
   token,lemma,tag,freq,relfreq=line[:-1].split('\t')
@@ -10,14 +11,23 @@ outfnc=open('nc_frequencies.txt','w')
 outfag=open('ag_frequencies.txt','w')
 outfvm=open('vm_frequencies.txt','w')
 outfas=open('as_frequencies.txt','w')
+nc=[]
+ag=[]
+vm=[]
 for lexeme,freq in sorted(lexemefreq.items(),key=lambda x:-x[1]):
   outf.write(lexeme+'\t'+str(freq)+'\n')
   if lexeme.endswith('#Nc'):
     outfnc.write(lexeme+'\t'+str(freq)+'\n')
+    if freq>=3000:
+      nc.append((lexeme,freq))
   elif lexeme.endswith('#Ag'):
     outfag.write(lexeme+'\t'+str(freq)+'\n')
+    if freq>=3000:
+      ag.append((lexeme,freq))
   elif lexeme.endswith('#Vm'):
     outfvm.write(lexeme+'\t'+str(freq)+'\n')
+    if freq>=3000:
+      vm.append((lexeme,freq))
   elif lexeme.endswith('#As'):
     outfas.write(lexeme+'\t'+str(freq)+'\n')
 outf.close()
@@ -25,3 +35,12 @@ outfnc.close()
 outfag.close()
 outfvm.close()
 outfas.close()
+shuffle(nc)
+shuffle(ag)
+shuffle(vm)
+def writedown(l,f):
+  for lexeme,freq in l:
+    f.write(lexeme+'\t'+str(freq)+'\n')
+writedown(nc,open('nc_shuf.txt','w'))
+writedown(ag,open('ag_shuf.txt','w'))
+writedown(vm,open('vm_shuf.txt','w'))
