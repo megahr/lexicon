369b56ad7e4321191ad15fa9a425a7c136486c21
Nikola Ljubešić <nljubesi@gmail.com>
2017-03-10 21:21:13 +0100

    Added generated docs

---
+++ generate_docs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+ag=[e.split('\t')[0] for e in open('ag_shuf_1000.txt')]
+nc=[e.split('\t')[0] for e in open('nc_shuf_1000.txt')]
+vm=[e.split('\t')[0] for e in open('vm_shuf_1000.txt')]
+from random import shuffle
+all=ag+nc+vm
+shuffle(all)
+for i in range(30):
+	docid=str(i+1).zfill(2)
+	open('docs/'+docid+'a.txt','w').write('\n'.join(all[i*100:(i+1)*100])+'\n')
+	open('docs/'+docid+'b.txt','w').write('\n'.join(reversed(all[i*100:(i+1)*100]))+'\n')
+	
